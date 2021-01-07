fa49ba99ffb3f4936f1dc0ea3e908257d6eaa335
jiashunx <wenjiashun@163.com>
2021-01-07 12:45:10 +0800

    get content-type by request url suffix.

--- masker-rest-framework/src/main/java/io/github/jiashunx/masker/rest/framework/filter/StaticResourceFilter.java
+++ masker-rest-framework/src/main/java/io/github/jiashunx/masker/rest/framework/filter/StaticResourceFilter.java
@@ -5,6 +5,8 @@ import io.github.jiashunx.masker.rest.framework.MRestRequest;
 import io.github.jiashunx.masker.rest.framework.MRestResponse;
 import io.github.jiashunx.masker.rest.framework.cons.Constants;
 import io.github.jiashunx.masker.rest.framework.model.StaticResource;
+import io.github.jiashunx.masker.rest.framework.serialize.MRestSerializer;
+import io.github.jiashunx.masker.rest.framework.util.IOUtils;
 import io.github.jiashunx.masker.rest.framework.util.MRestHeaderBuilder;
 import io.github.jiashunx.masker.rest.framework.util.StaticResourceHolder;
 
@@ -15,12 +17,21 @@ import java.util.*;
  */
 public class StaticResourceFilter implements MRestFilter {
 
-    private final MRestContext restContext;
     private final StaticResourceHolder staticResourceHolder;
 
+    private static final String DEFAULT_CONTENT_TYPE_KEY = ".*";
+    private static final Map<String, String> CONTENT_TYPE_MAP = new HashMap<>();
+    static {
+        String json = IOUtils.loadContentFromClasspath("masker-rest/content-type.json");
+        Map<?, ?> map = MRestSerializer.jsonToObj(json, Map.class);
+        map.forEach((key, value) -> {
+            CONTENT_TYPE_MAP.put(String.valueOf(key), String.valueOf(value));
+        });
+    }
+
     public StaticResourceFilter(MRestContext restContext) {
-        this.restContext = Objects.requireNonNull(restContext);
-        this.staticResourceHolder = new StaticResourceHolder(this.restContext);
+        MRestContext restContext1 = Objects.requireNonNull(restContext);
+        this.staticResourceHolder = new StaticResourceHolder(restContext1);
     }
 
     public void reloadClasspathResource(Map<String, List<String>> pathMap) {
@@ -41,6 +52,15 @@ public class StaticResourceFilter implements MRestFilter {
             String contentType = restRequest.getAcceptFirst();
             if (contentType == null) {
                 contentType = resource.getContentType();
+            }
+            String suffix = DEFAULT_CONTENT_TYPE_KEY;
+            int index = requestUrl.lastIndexOf(".");
+            if (index > 0) {
+                suffix = requestUrl.substring(index);
+            }
+            String _contentType = CONTENT_TYPE_MAP.get(suffix);
+            if (_contentType != null) {
+                contentType = _contentType;
             }
             restResponse.write(bytes, MRestHeaderBuilder.Build(Constants.HTTP_HEADER_CONTENT_TYPE, contentType));
             return;
