13b1ee172111121751ddac89244c09e911c64a92
jiashunx <wenjiashun@163.com>
2021-01-25 10:56:05 +0800

    add AbstractRestServlet

---
+++ masker-rest-framework/src/main/java/io/github/jiashunx/masker/rest/framework/servlet/AbstractRestServlet.java
@@ -0,0 +1,16 @@
+package io.github.jiashunx.masker.rest.framework.servlet;
+
+import io.github.jiashunx.masker.rest.framework.MRestRequest;
+import io.github.jiashunx.masker.rest.framework.MRestResponse;
+
+/**
+ * @author jiashunx
+ */
+public abstract class AbstractRestServlet implements MRestServlet {
+
+    @Override
+    public void service(MRestRequest restRequest, MRestResponse restResponse) {
+
+    }
+
+}
